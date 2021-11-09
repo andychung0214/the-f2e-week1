cd92a4f49aed168525c4faf3b49b302c8f66e171
Andy Chung <andy6756@gmail.com>
2021-11-09 15:16:31 +0800

    Remove Old Folder and File

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LayoutComponent } from './layout/layout.component';
-import { AttractionsComponent } from './attractions/attractions.component';
-import { TransportComponent } from './transport/transport.component';
-import { CuisinesComponent } from './cuisines/cuisines.component';
 
 import { TravelListComponent } from './travel/travel-list/travel-list.component';
 import { RecentActivitiesComponent } from './travel/recent-activities/recent-activities.component';
@@ -17,18 +14,6 @@ const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children: [
-      {
-        path: 'attractions',
-        component: AttractionsComponent
-      },
-      {
-        path: 'transport',
-        component: TransportComponent
-      },
-      {
-        path: 'cuisines',
-        component: CuisinesComponent
-      },
       {
         path: 'travel-list',
         component: TravelListComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './header/header.component';
-import { TransportComponent } from './transport/transport.component';
-import { AttractionsComponent } from './attractions/attractions.component';
-import { CuisinesComponent } from './cuisines/cuisines.component';
 
 import { TravelHeaderComponent } from './travel/travel-header/travel-header.component';
 import { TravelListComponent } from './travel/travel-list/travel-list.component';
@@ -23,10 +19,6 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     AppComponent,
     LayoutComponent,
-    HeaderComponent,
-    TransportComponent,
-    AttractionsComponent,
-    CuisinesComponent,
     TravelListComponent,
     PopularDestinationComponent,
     RecentActivitiesComponent,
