619e2535523e185a13b611970bc64222e7f06346
Andy Chung <andy6756@gmail.com>
2021-11-14 01:27:25 +0800

    Update City Select Name

--- src/app/shared-material/mat-dialog-component/mat-dialog-component.component.ts
+++ src/app/shared-material/mat-dialog-component/mat-dialog-component.component.ts
@@ -50,47 +50,47 @@ export class MatDialogComponentComponent implements OnInit {
   ];
 
   categoryList: Category[] = [
-    {value: '熱門景點', viewValue: '熱門景點'},
-    {value: '熱門美食', viewValue: '熱門美食'},
-    {value: '近期活動', viewValue: '近期活動'}
+    { value: '熱門景點', viewValue: '熱門景點' },
+    { value: '熱門美食', viewValue: '熱門美食' },
+    { value: '近期活動', viewValue: '近期活動' }
   ]
 
-  taipeiList: Area[] =[
-      {value: '台北', viewValue: '台北'},
-      {value: '新北', viewValue: '新北'},
-      {value: '基隆', viewValue: '基隆'},
-      {value: '桃園', viewValue: '桃園'},
-      {value: '竹市', viewValue: '竹市'},
-      {value: '竹縣', viewValue: '竹縣'},
-      {value: '宜蘭', viewValue: '宜蘭'}
+  taipeiList: Area[] = [
+    { value: '基隆市', viewValue: '基隆市' },
+    { value: '臺北市', viewValue: '臺北市' },
+    { value: '新北市', viewValue: '新北市' },
+    { value: '桃園市', viewValue: '桃園市' },
+    { value: '新竹縣', viewValue: '新竹縣' },
+    { value: '新竹市', viewValue: '新竹市' },
   ];
 
-  centralList: Area[] =[
-    {value: '苗栗', viewValue: '苗栗'},
-    {value: '台中', viewValue: '台中'},
-    {value: '彰化', viewValue: '彰化'},
-    {value: '南投', viewValue: '南投'},
-    {value: '雲林', viewValue: '雲林'}
-];
+  centralList: Area[] = [
+    { value: '苗栗縣', viewValue: '苗栗縣' },
+    { value: '台中市', viewValue: '台中市' },
+    { value: '彰化縣', viewValue: '彰化縣' },
+    { value: '南投縣', viewValue: '南投縣' },
+    { value: '雲林縣', viewValue: '雲林縣' }
+  ];
 
-southList: Area[] =[
-  {value: '嘉市', viewValue: '嘉市'},
-  {value: '嘉縣', viewValue: '嘉縣'},
-  {value: '台南', viewValue: '台南'},
-  {value: '高雄', viewValue: '高雄'},
-  {value: '屏東', viewValue: '屏東'},
-  {value: '澎湖', viewValue: '澎湖'}
-];
+  southList: Area[] = [
+    { value: '嘉義縣', viewValue: '嘉義縣' },
+    { value: '嘉義市', viewValue: '嘉義市' },
+    { value: '臺南市', viewValue: '臺南市' },
+    { value: '高雄市', viewValue: '高雄市' },
+    { value: '屏東縣', viewValue: '屏東縣' }
+  ];
 
-eastList: Area[] =[
-  {value: '花蓮', viewValue: '花蓮'},
-  {value: '臺東', viewValue: '臺東'}
-];
+  eastList: Area[] = [
+    { value: '宜蘭縣', viewValue: '宜蘭縣' },
+    { value: '花蓮縣', viewValue: '花蓮縣' },
+    { value: '臺東縣', viewValue: '臺東縣' }
+  ];
 
-outIslandList: Area[] =[
-  {value: '金門', viewValue: '金門'},
-  {value: '連江', viewValue: '連江'}
-];
+  outIslandList: Area[] = [
+    { value: '連江縣', viewValue: '連江縣' },
+    { value: '金門縣', viewValue: '金門縣' },
+    { value: '澎湖縣', viewValue: '澎湖縣' }
+  ];
 
   animal: string;
   name: string;
